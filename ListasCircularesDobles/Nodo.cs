1ab6bc0b9b259f10fb289cef93f45c22425ad74d
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-05-01 16:18:31 -0700

    Creamos la Clase "Nodo"

---
+++ ListasCircularesDobles/Nodo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasCircularesDobles
+{
+    public class Nodo
+    {
+        private int Dato;
+        private Nodo Siguiente;
+        private Nodo Anterior;
+
+        public int dato
+        {
+            get { return dato; }
+            set { dato = value; }
+        }
+
+        public Nodo siguiente
+        {
+            get { return siguiente; }
+            set { siguiente = value; }
+        }
+
+        public Nodo anterior
+        {
+            get { return anterior; }
+            set { anterior = value; }
+        }
+    }
+
+}

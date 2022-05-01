a1df23bf7fb1fe72b84dd9237a23c0e67b4289c4
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-05-01 12:20:08 -0700

    Creamos el constructor "InsertarNodo"

--- ListasCirculares/ListaCircular.cs
+++ ListasCirculares/ListaCircular.cs
@@ -16,6 +16,25 @@ namespace ListasCirculares
             primero = null;
             ultimo = null;
         }
+
+        public void InsertarNodo()
+        {
+            Nodo nuevo = new Nodo();
+            Console.WriteLine("Insertar un nuevo dato");
+            nuevo.dato = int.Parse(Console.ReadLine());
+            if(primero == null)
+            {
+                primero = nuevo;
+                primero.siguiete = primero;
+                ultimo = primero;
+            }
+            else
+            {
+                ultimo.siguiete = nuevo;
+                nuevo.siguiete = primero;
+                ultimo = nuevo;
+            }
+        }
     }
 
 }

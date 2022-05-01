86fbd496aaeb97e7b010b03de1b008016f6721b7
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-05-01 12:08:03 -0700

    Constructor "Lista" nos va a decir primero null

---
+++ ListasCirculares/ListaCircular.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasCirculares
+{
+    public class Lista
+    {
+        private Nodo primero = new Nodo();
+        private Nodo ultimo = new Nodo();
+
+        public Lista()
+        {
+            primero = null;
+            ultimo = null;
+        }
+    }
+
+}

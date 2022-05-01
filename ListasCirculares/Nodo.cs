dc1ee3f9e94b7cacf7683570c0ede7a5ca8836e5
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-05-01 12:01:26 -0700

    Agremos clase "Nodo"

---
+++ ListasCirculares/Nodo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasCirculares
+{
+    public class Nodo
+    {
+        private int Dato;
+        private Nodo siguiente;
+
+        public int dato
+        {
+            get { return Dato; }
+            set { Dato = value; }
+        }
+
+        public Nodo siguiete
+        {
+            get { return siguiente; }
+            set { siguiente = value; } 
+        }
+    }
+}

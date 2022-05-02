b9cd28fd6d5314299caefc7dbab660e51cdcdac5
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-05-01 17:23:15 -0700

    Constructor "Eliminar Nodo" esta Hecho

--- ListasCircularesDobles/Program.cs
+++ ListasCircularesDobles/Program.cs
@@ -12,3 +12,12 @@ Console.WriteLine("-----------");
 lista.DesplegarNodoP();
 Console.WriteLine("---------");
 lista.DesplegarNodoU();
+
+lista.BuscarNodo();
+Console.WriteLine("---------");
+
+lista.ModificarNodo();
+lista.DesplegarNodoP();
+
+lista.EliminarNodo();
+lista.DesplegarNodoP();
